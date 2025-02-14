9b86fdb063d52c0827185959780a774baa1f2943
egor <egormalov.2007@gmail.com>
2025-02-14 20:43:35 +1000

    №01. Entities added, Actuator added.

---
+++ src/main/java/com/cerberus/avanzanaturaldentityservice/model/UserCredential.java
@@ -0,0 +1,36 @@
+package com.cerberus.avanzanaturaldentityservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Table(schema = "users", name = "user")
+@NoArgsConstructor
+public class UserCredential {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+}
